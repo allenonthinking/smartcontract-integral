698aad22d4cbe6a503adf257f22fc8acbc1898fe
allenliu <223woai@163.com>
2017-11-15 16:44:42 +0800

    add spring mvc Dao

--- src/main/java/io/allen/modules/generator/dao/BizGiftDao.java
+++ src/main/java/io/allen/modules/generator/dao/BizGiftDao.java
@@ -3,6 +3,8 @@ package io.allen.modules.generator.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
 import io.allen.modules.generator.entity.BizGiftEntity;
 import io.allen.modules.sys.dao.BaseDao;
 
@@ -17,4 +19,6 @@ public interface BizGiftDao extends BaseDao<BizGiftEntity> {
 	List<BizGiftEntity> queryExchangeList(Map<String, Object> map);
 	
 	int queryExchangeTotal(Map<String, Object> map);
+	
+	int updateTotal(@Param("giftId")Long giftId, @Param("count")Integer count);
 }
